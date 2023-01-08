b2349e67b4714f9ab4c2ad57f88fd559a6217479
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-08 23:46:59 +0530

    to compute quotient and remainder of numbers

---
+++ QuotientRemainder/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Two Numbers : ");
+        Console.WriteLine("Enter Dividend Number ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Divisor Number");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        int result1 = num1 / num2;
+        int result2 = num1 % num2;
+        Console.WriteLine("Quotient is "+result1);
+        Console.WriteLine("Remainder is " + result2);
+    }
+}
