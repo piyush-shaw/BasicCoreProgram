748886c7bbb374a3e5d54e7de90fd400aa017fa4
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-08 22:44:27 +0530

    print the table of power of two

---
+++ PowerOfTwo/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the N value ");
+        int powerOfTwo = Convert.ToInt32(Console.ReadLine());
+        if (powerOfTwo <= 0 || powerOfTwo < 31)
+        {
+            Console.WriteLine("Power of 2's ");
+            for(int i = 0; i <= powerOfTwo; i++)
+            {
+                int value = ((int)Math.Pow(2, i)); //2^i
+                Console.WriteLine("The of value 2^"+i+" is "+value);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Value overflow int value");
+        }
+    }
+}
