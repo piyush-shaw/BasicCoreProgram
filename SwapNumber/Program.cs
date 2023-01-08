bb80313320971943e0fc4385d2fe9e75e54a3bff
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-08 23:59:18 +0530

    swapping of two number

---
+++ SwapNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int x, y, s;
+        Console.WriteLine("Enter the value of X ");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the value of Y ");
+        y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Before swapping the number are "+x+" "+y);
+        s = x;
+        x = y;
+        y = s;
+        Console.WriteLine("Afer swapping the number are "+x+" "+y);
+    }
+}
