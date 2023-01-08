e64d855293582937e87ad8ab9e899d0bdef8d45c
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-09 00:07:34 +0530

    to check number is even or odd

---
+++ EvenOdd/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("Number is even");
+        }
+        else
+        {
+            Console.WriteLine("Number is odd ");
+        }
+    }
+}
