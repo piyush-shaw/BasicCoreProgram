d329123d96d659bb90fd8eaa9c28d5be540fe735
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-08 23:03:19 +0530

    sum of harmonic series up to terms

---
+++ HarmonicNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        double sum = 0;
+        Console.WriteLine("Enter number of terms ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Harmonic series is");
+        for (int i = 1; i <= num; i++)
+        {
+            Console.WriteLine("1/"+i);
+            sum += 1 / (float)i;
+            Console.WriteLine(sum);
+        }
+        Console.WriteLine("Sum of harmonic series upto "+num+" terms "+sum);
+    }
+}
