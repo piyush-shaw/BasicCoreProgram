c1a7b01411e4786f9b91885988581c2360b07352
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-08 23:26:00 +0530

    prime factors of a number

---
+++ Factor/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number to find prime factorization ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        for(int i = 1; i <= num; i++)
+        {
+            if (num % i == 0)
+            {
+                Console.WriteLine("The prime factors of "+num+" is "+i);
+            }
+        }
+    }
+}
