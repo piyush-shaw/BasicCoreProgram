9d58b4720cd9d561609209187326c7253a0168d3
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-08 22:20:32 +0530

    year is a leap year or not

---
+++ LeapYear/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the Year ");
+        int year = Convert.ToInt32(Console.ReadLine());
+        if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0)
+        {
+            Console.WriteLine(year+" is a Leap Year");
+        }
+        else
+        {
+            Console.WriteLine(year+" is not a Leap Year");
+        }
+    }
+}
