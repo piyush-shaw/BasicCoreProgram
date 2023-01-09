71ec190417b63332206b0ac30f2d28cf54c59c30
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-09 11:04:31 +0530

    to check largest number among three numbers

---
+++ LargestNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter 3 Number ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+        if (a > b && a > c)
+        {
+            Console.WriteLine(a+" is the largest number ");
+        }
+        else if (b > a && b > c)
+        {
+            Console.WriteLine(b+" is the largest number ");
+        }
+        else
+        {
+            Console.WriteLine(c+" is the largest number ");
+        }
+    }
+}
