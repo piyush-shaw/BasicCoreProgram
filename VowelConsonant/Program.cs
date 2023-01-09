5a9ce9c7fda96dda4578dbca77e54cac722b8bbc
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-09 10:45:18 +0530

    to check an alphabet is vowel or consonant

---
+++ VowelConsonant/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+
+    {
+        char ch ;
+        Console.WriteLine("Enter any character");
+        ch = Convert.ToChar(Console.ReadLine());
+        if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U')
+        {
+            Console.WriteLine(ch+" is a Vowel ");
+        }
+        else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+        {
+            Console.WriteLine(ch+" is Consonant");
+        }
+    }
+}
